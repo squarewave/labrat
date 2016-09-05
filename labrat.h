3e27842a37e16489573e8b4acdd51fb40556bdb5
Doug Thayer <dougathayer@gmail.com>
2016-09-05 14:13:52 -0700

    updated in-line documentation

--- labrat.h
+++ labrat.h
@@ -8,41 +8,74 @@
 //  find tests in the current directory and build a list of them that it can
 //  consume.
 //
-//      Example:
+//  Here's an example:
 //
-//------// inside calculator.h:
+//  ```c
+//  // inside calculator.c:
 //
-//      ...
+//  ...
 //
-//      TEST_CASE(test_calculates)
-//      {
-//          const char* calc_str = "15 5 +";
-//          int result = calculate(calc_str);
+//  TEST_CASE(test_calculates)
+//  {
+//      const char* calc_str = "15 5 +";
+//      int result = calculate(calc_str);
 //
-//          ASSERT_EQ(20, result, "%d"); // expected, actual, format
-//      }
+//      ASSERT_EQ(result, 20, "%d");  // actual, expected, format
+//  }
+//  ```
 //
-//------// in the file that defines main()
+//  ```c
+//  // inside main.c
 //
-//      #define LR_IMPLEMENTATION
-//      #include "labrat.h"
+//  #define LR_IMPLEMENTATION
+//  #include "labrat.h"
 //
-//      int main(int argc, char const *argv[])
-//      {
-//          LR_PRELUDE(argc, argv);
-//          
-//          ... // do what we would do if tests didn't run.
-//      }
-//------
+//  int main(int argc, char const *argv[])
+//  {
+//      LR_PRELUDE(argc, argv);
 //
-//  Note:
-//      - the ASSERT functions do not guarantee single execution, so avoid
-//        passing in parameters with side-effects.
+//      ... // do what we would do if tests didn't run.
+//  }
+//  ```
 //
-//  Todo:
-//      - allow arguments to filter the files searched by the test-finder
-//      - support platforms other than Windows
+//  ### Using GCC:
+//  ```sh
+//  # generate the labrat executable
+//  gcc -x c labrat.h -D LR_GEN_EXECUTABLE -o labrat
 //
+//  # run labrat to generate test metadata
+//  ./labrat
+//
+//  # compile our program
+//  gcc program.c calculator.c -o program
+//
+//  # run the tests
+//  ./program --lr-run-tests
+//
+//  # run benchmarks
+//  ./program --lr-run-benchmarks 1000
+//
+//  ```
+//
+//  ### Using MSVC (command line):
+//  ```bat
+//  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+//  cl /TC /DLR_GEN_EXECUTABLE labrat.h
+//  labrat.exe
+//  cl program.c calculator.c
+//  program.exe --lr-run-tests
+//  program.exe --lr-run-benchmarks 1000
+//  ```
+//
+//  ## How it Works
+//
+//  It works by recursively grabbing every source and header file in the
+//  directory, tokenizing them, and searching for the `TEST_CASE` and
+//  `BENCHMARK` tokens. It then builds a `labrat_data.c` file which includes
+//  all of the necessary information for running the tests. Then when
+//  you want to run your tests, it just includes this file in such a way
+//  that it generates code which handles running the tests.
+
 
 /******************************************************************************/
 ///////////////////////////////// BEGIN HEADER /////////////////////////////////
